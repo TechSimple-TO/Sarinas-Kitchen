0811ad329ca69ebd4937315913d8339d7b81983c
unknown <admin@techsimple-to.ca>
2025-10-15 12:33:10 -0400

    x

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sarinas-kitchen/', // <-- repo name
+  base: '/Sarinas-Kitchen/', // <-- repo name
 });
 // This configuration sets up Vite for a React project with the base path set to the repository name.
